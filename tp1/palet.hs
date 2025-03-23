c94eccb94db2e36474a318207b70b597ce25b483
Agustina Videla Rivero <avidelarivero@udesa.edu.ar>
2025-03-23 18:44:13 -0300

    Ultimos detalles

--- tp1/palet.hs
+++ tp1/palet.hs
@@ -4,7 +4,7 @@ module Palet ( Palet, newP, destinationP, netP )
 data Palet = Pal String Int deriving (Eq, Show)
 
 -- No permitimos la creacion de palets con peso 0 o negativo, o con peso mayor a 10 porque no se pueden cargar en ninguna bahia
-newP :: String -> Int -> Palet
+newP :: String -> Int -> Palet -- construye un Palet dada una ciudad de destino y un peso en toneladas
 newP ciudad_destino peso
      | peso <= 0 = error "Error: El peso del palet debe ser mayor a 0"
      | peso > 10 = error "Error: El peso del palet no puede superar las 10 toneladas"
