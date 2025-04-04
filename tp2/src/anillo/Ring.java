eb713fd9033c061beea2fda8f78f6eafb0bf3e8b
trinidad pujol <tpujolsierra@udesa.edu.ar>
2025-04-04 03:33:04 -0300

    fallan 3 tests

--- tp2/src/anillo/Ring.java
+++ tp2/src/anillo/Ring.java
@@ -6,25 +6,34 @@ public class Ring {
     private List<Object> elements;
     private int currentIndex;
 
+    // Constructor
+    public Ring() {
+        this.elements = new ArrayList<>(); // Inicializa la lista vacía con el currentIndex en -1
+        this.currentIndex = -1;
+    }
+
     public Ring next() {
-        if (elements.isEmpty()) {
-            throw new RuntimeException("El anillo está vacío."); 
+        if (elements.isEmpty() || currentIndex == -1) {    // Caso anillo vacío
+            throw new RuntimeException("El anillo está vacío, no hay elemento siguiente"); 
+        } else {
+            currentIndex = (currentIndex + 1) % elements.size();  // Avanza de forma circular
+            return this; 
         }
-        currentIndex = (currentIndex + 1) % elements.size();  // Avanza de forma circular
-        return this; 
     }
 
     public Object current() {
         if (elements.isEmpty() || currentIndex == -1) {     // Caso anillo vacio 
-            throw new RuntimeException("No hay elemento actual"); 
+            throw new RuntimeException("El anillo está vacío, no hay elemento actual"); 
         }
         return elements.get(currentIndex); 
     }
 
-    public Ring add( Object cargo ) {
+    public Ring add(Object cargo) {
         elements.add(cargo); 
         if (currentIndex == -1) { // Si es el primer elemento, establece el índice actual a 0
-            currentIndex = 0; 
+            currentIndex = 0;
+        } else {
+            currentIndex++;         // Actualiza el indice (se agrega el elemento al "final" del anillo)
         }
         return this;
     }
