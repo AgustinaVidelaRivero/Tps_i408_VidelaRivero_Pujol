61e7601e52400674fcff0b83044e12aa40e8e60d
Agustina Videla Rivero <avidelarivero@udesa.edu.ar>
2025-04-08 17:00:23 -0300

    Limpieza de estructura y agregado package

---
+++ tp2/src/anillo/Link.java
@@ -0,0 +1,97 @@
+package anillo;
+
+import java.util.Stack;
+import java.util.function.Supplier;
+
+abstract class Link {
+    public abstract Link next();
+    public abstract Link prev();
+    public abstract Object cargo();
+    public abstract Link add(Object cargo, Stack<Supplier<Link>> stack);
+    public abstract Link remove(Stack<Supplier<Link>> stack);
+}
+
+class EmptyLink extends Link {
+    public EmptyLink() {}
+
+    public Link next(){
+        throw new RuntimeException("El anillo está vacío, no hay elemento siguiente");
+    }
+
+    public Link prev(){
+        throw new RuntimeException("El anillo está vacío, no hay elemento anterior");
+    }
+
+    public Object cargo(){
+        throw new RuntimeException("El anillo está vacío, no hay valor cargado");
+    }
+
+    public Link add(Object cargo, Stack<Supplier<Link>> stack){
+        ValidLink newLink = new ValidLink(cargo);
+        stack.push(EmptyLink::new); //si se elimina el unico nodo --> pasamos al vacio
+        return new ValidLink(cargo);
+    }
+
+    public Link remove(Stack<Supplier<Link>> stack){
+        throw new RuntimeException("No hay elementos para eliminar");
+    }
+}
+
+class ValidLink extends Link {
+    public Object cargo;
+    public ValidLink prev;
+    public ValidLink next;
+
+    public ValidLink(Object cargo){
+        this.cargo = cargo;
+        this.next = this;
+        this.prev = this;
+    }
+
+    public Link next(){
+        return next;
+    }
+
+    public Link prev(){
+        return prev;
+    }
+
+    public Object cargo(){
+        return cargo;
+    }
+
+    public Link add(Object cargo, Stack<Supplier<Link>> stack) {
+        ValidLink newLink = new ValidLink(cargo);
+//        newLink.next = this;
+//        newLink.prev = this.prev;
+//
+//        this.prev.next = newLink;
+//        this.prev = newLink;
+
+        linkBeforeSelf(newLink);
+        stack.push(() -> this.next); // volvés al que está después del eliminado
+        return newLink; // el nuevo current pasa a ser el que agrego
+    }
+
+
+    public Link remove(Stack<Supplier<Link>> stack){
+
+        unlinkSelf();
+        return stack.pop().get();
+        }
+
+    private void unlinkSelf() {
+        this.prev.next = this.next;
+        this.next.prev = this.prev;
+    }
+
+    private void linkBeforeSelf(ValidLink newLink) {
+        newLink.next = this;
+        newLink.prev = this.prev;
+
+        this.prev.next = newLink;
+        this.prev = newLink;
+    }
+    }
+
+
--- tp2/src/anillo/Ring.java
+++ tp2/src/anillo/Ring.java
@@ -1,81 +1,34 @@
 package anillo;
 
-class Link {
-    Object cargo;  
-    Link prev;
-    Link next;     
-
-    Link(Object cargo) {
-        this.cargo = cargo;  
-        this.prev = null; 
-        this.next = null; 
-    }
-}
+import java.util.Stack;
+import java.util.function.Supplier;
 
 public class Ring {
     private Link current; // Eslabón actual
-    private int size;     // Contador de elementos en el anillo
+    private Stack<Supplier<Link>> stack;
 
     // Constructor
     public Ring() {
-        this.current = null;
-        this.size = 0;
+        this.current = new EmptyLink();
+        this.stack = new Stack<>();
     }
 
     public Ring next() {
-        if (current == null) { // Si no hay elementos, no podemos avanzar
-            throw new RuntimeException("El anillo está vacío, no hay elemento siguiente");
-        }
-        current = current.next; 
+        current = current.next();
         return this;
     }
 
     public Object current() {
-        if (current == null) { // Si no hay elementos, no hay un elemento actual
-            throw new RuntimeException("El anillo está vacío, no hay elemento actual");
-        }
-        return current.cargo; // Devuelve el valor del eslabón actual
+        return current.cargo(); // Devuelve el valor del eslabón actual
     }
 
     public Ring add(Object cargo) {
-        Link newLink = new Link(cargo); // Crea un nuevo eslabón
-        if (current == null) { // Si el anillo estaba vacío
-            current = newLink; // Establece el eslabón actual al nuevo eslabón
-            current.next = current; // Hace referencia a sí mismo, creando un anillo circular
-        }
-        if (current.next == current) {    // si solo hay un elemento
-            current.next = newLink;
-            current.prev = newLink;
-            newLink.next = current;
-            newLink.prev = current;
-            current = newLink;  
-        } else {
-            // Se agregan enlaces por "izquierda" (cambiando el prev en vez del next)
-            current.prev.next = newLink;
-            newLink.next = current;
-            newLink.prev = current.next;
-            current = newLink;
-        }
-        size++;
-        return this; 
+        current = current.add(cargo, stack);
+        return this;
     }
 
     public Ring remove() {                            // el current pasa a ser el next
-        if (current == null) { // Si está vacío 
-            throw new RuntimeException("No hay elementos para eliminar");
-        }
-        if (current.next == current) {  // Si hay solo un elemento
-            current = null;
-        } else {
-            current.next.next = current.prev;
-            current.prev.next = current.next;
-            next();                         
-        }
-        size--; 
-        return this; 
-    }
-    
-    public int size() {
-        return size; 
+        current = current.remove(stack);
+        return this;
     }
 }
